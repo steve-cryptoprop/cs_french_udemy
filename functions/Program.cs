21cc4a1221edf2eca750295a78940ec8ea1cd3a4
StevenMaharaj <stevesamah@gmail.com>
2021-06-24 15:12:46 +1000

    functions

---
+++ functions/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace functions
+{
+    class Program
+    {
+        static string nom = "steve";
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Lancer");
+            DireBon(nom);
+        }
+
+        static void DireBon(string nom)
+        {
+            Console.WriteLine("BOnjour" + nom);
+        }
+    }
+}

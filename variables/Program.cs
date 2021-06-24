334b5cb667d11b35fd972ede63148cdd88096f86
StevenMaharaj <stevesamah@gmail.com>
2021-06-24 14:51:52 +1000

    second commit

---
+++ variables/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace variables
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            float score;
+            bool active = false;
+            score = 12.5f;
+            // age = 69;
+            active = true;
+            name = "steve";
+            Console.WriteLine("Hello " + name);
+            string nb = "10"; 
+            int res = int.Parse(nb);
+
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Age = " + age.ToString());
+        }
+    }
+}
